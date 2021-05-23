12ed7cd57f56a2dcd620ed47c85ccfb4b6f850bd
Zekeriyya Yunus Sönmez <83361421+Zekeriyya232@users.noreply.github.com>
2021-05-23 20:05:48 +0300

    Add files via upload

--- Yunus_Sönmez/takım.js
+++ Yunus_Sönmez/takım.js
@@ -8,7 +8,7 @@ let data = fetch("https://fly.sportsdata.io/v3/nba/scores/json/teams?key=b0595dc
         if(sira<30) {
             var isim = element["Name"]
             var logo =element["WikipediaLogoUrl"]
-            takimlar.innerHTML += `<li><img src="${logo}" style= "width:50px ;margin: 10px"><b>${isim}</b></li>`
+            takimlar.innerHTML += `<span><img src="${logo}" style= "width:50px ;margin: 10px"><b>${isim}</b></span>`
         }
         sira++
     });
